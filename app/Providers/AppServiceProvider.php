36571289ebefc8919f5967b9cdeb0e86b515cbd5
David <davidguilarte7@gmail.com>
2020-06-09 18:06:25 -0430

    Changed whole application locale

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,26 +7,27 @@ use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        Schema::defaultStringLength(191);
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $public_path = public_path();
-        app()->bind('path.public', function() use ($public_path){
-          return env('PUBLIC_PATH', $public_path);
-        });
-    }
+  /**
+  * Register any application services.
+  *
+  * @return void
+  */
+  public function register()
+  {
+    Schema::defaultStringLength(191);
+  }
+  
+  /**
+  * Bootstrap any application services.
+  *
+  * @return void
+  */
+  public function boot()
+  {
+    setlocale(LC_ALL, 'es_ES');
+    $public_path = public_path();
+    app()->bind('path.public', function() use ($public_path){
+      return env('PUBLIC_PATH', $public_path);
+    });
+  }
 }
