a449646197b3876f04ab536b86642fa097489617
David <davidguilarte7@gmail.com>
2020-06-10 11:10:34 -0430

    Added fillable fields to Owner model

--- app/Owner.php
+++ app/Owner.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Owner extends Model
 {
+  protected $fillable = ['name', 'email', 'phone'];
+  
   public function apartments(){
     return $this->hasMany('App\Apartment');
   }
